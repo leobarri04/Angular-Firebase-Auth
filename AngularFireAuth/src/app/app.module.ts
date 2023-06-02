3ee96967b2705b294cc97dd980eb9bef07537175
Lbarrientos <lbarrientos@rruhllaw.com>
2023-06-02 00:12:53 -0600

    Adding firebase connection

--- AngularFireAuth/src/app/app.module.ts
+++ AngularFireAuth/src/app/app.module.ts
@@ -1,10 +1,14 @@
+//Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Material/material.module';
+import { AngularFireModule } from '@angular/fire/compat';
+
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,8 @@ import { MaterialModule } from './Material/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AngularFireAuth/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAPF3_hgINZrLI0HFkRFQU6iIRAUXCOTGM',
+    authDomain: 'angularfireauthapp-7c5a4.firebaseapp.com',
+    projectId: 'angularfireauthapp-7c5a4',
+    storageBucket: 'angularfireauthapp-7c5a4.appspot.com',
+    messagingSenderId: '1019864715956',
+    appId: '1:1019864715956:web:c9aaa2c445a89d38ba3de1',
+    measurementId: 'G-5FQ17NZDWP',
+  }
+};
